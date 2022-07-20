de33e88d0cb16e2576e7c89510d362a4cc32e1e0
noname <48761044+noname0310@users.noreply.github.com>
2022-07-21 04:26:09 +0900

    fix useEffect bug

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,21 +1,32 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Game } from "the-world-engine";
 import "./App.css";
 import { TestGameBootstrapper } from "./game/TestGameBootstrapper";
 
 function App(): JSX.Element {
     const div = useRef<HTMLDivElement>(null);
+    const [game, setGame] = useState<Game|null>(null);
 
-    useEffect(() => { //on component mounted
-        if (!div.current) throw new Error("unreachable");
-        const game = new Game(div.current);
-        game.run(TestGameBootstrapper);
-        game.inputHandler.startHandleEvents();
+    useEffect(() => {
+        if (!div.current) return;
 
-        return () => { //on component unmount
+        if (game) {
+            game.inputHandler.stopHandleEvents();
             game.dispose();
+            setGame(null);
+        }
+
+        const newGame = new Game(div.current);
+        newGame.run(TestGameBootstrapper);
+        newGame.inputHandler.startHandleEvents();
+        setGame(newGame);
+
+        return () => {
+            newGame.inputHandler.stopHandleEvents();
+            newGame.dispose();
+            setGame(null);
         };
-    }, []);
+    }, [div]);
 
     return (<div className="App" ref={div}/>);
 }
